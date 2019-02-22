bfb0e734efb44c4ed85d8f7b647b760cf0150ab6
acnaman <acnadin01@gmail.com>
2019-02-23 01:35:52 +0900

    add sigmoid method

--- python/deeplearning/neuron.py
+++ python/deeplearning/neuron.py
@@ -1,6 +1,10 @@
 import numpy as np
 
+# 引数x：numpy.array()型
 def step_function(x):
     y = x>0 
     return y.astype(np.int)
-    
+
+# 引数x：numpy.array()型
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
