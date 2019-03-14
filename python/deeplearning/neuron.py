d284f2a0f3d97d5aa0562145ef38d56968bae5c2
acnaman <acnadin01@gmail.com>
2019-03-14 12:57:59 +0900

    add neuron error function

--- python/deeplearning/neuron.py
+++ python/deeplearning/neuron.py
@@ -26,3 +26,12 @@ def softmax(a):
 
     return y
 
+# 2乗和誤差
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y-t)**2)
+
+# 交差エントロピー誤差
+def cross_entropy_error(y,t):
+    delta = 1e-7
+    return -np.sum(t * np.log(y.delta))
+
