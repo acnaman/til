fc42580439bda91f166993e9d48bc25a4dac9f61
acnaman <acnadin01@gmail.com>
2019-03-23 01:39:11 +0900

    add object nou dir

---
+++ ruby/object_nou/gyoumu_spec.rb
@@ -0,0 +1,27 @@
+# coding: utf-8
+require_relative 'gyoumu'
+
+describe Gyoumu do
+    let(:gyoumu) { Gyoumu.new }
+
+    example '社員はとりあえず起立します' do
+        expect(gyoumu.standup).to eq '社員はとりあえず起立する。'
+    end
+
+    describe TnatoGyoumu do
+        let(:gyoumu) { TantoGyoumu.new }
+
+        example '担当は慌てて起立します' do
+            expect(gyoumu.standup).to eq '担当は慌てて起立しました。'
+        end
+    end
+
+    describe ShuninGyoumu do
+        let(:gyoumu) { ShuninGyoumu.new }
+
+        example '主任は素早く起立します' do
+            expect(gyoumu.standup).to eq '主任は素早く立ちました。'
+        end
+    end
+end
+
