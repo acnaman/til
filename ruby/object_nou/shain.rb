296c53168cf1f09e9ae66197f2e4ed9f5338f82b
acnaman <acnadin01@gmail.com>
2019-03-26 12:48:40 +0900

    complete State pattern

--- ruby/object_nou/shain.rb
+++ ruby/object_nou/shain.rb
@@ -1,12 +1,27 @@
 # coding: utf-8
 require_relative 'gyoumu'
+require_relative 'yakushoku'
 
 # Shain class
 class Shain
     attr_accessor :gyoumu
+    attr_accessor :yakushoku
 
     def initialize
         @gyoumu = Gyoumu.new
+        @yakushoku = Yakushoku.new
+    end
+
+    def calculate_salary(kihonkyu)
+        @yakushoku.calculate_salary(kihonkyu)
+    end
+
+    def up
+        @yakushoku = @yakushoku.up
+    end
+
+    def down
+        @yakushoku = @yakushoku.down
     end
 
     def work
--- ruby/object_nou/yakushoku.rb
+++ ruby/object_nou/yakushoku.rb
@@ -1,3 +1,31 @@
 # coding: utf-8
 require_relative 'shain'
 
+# Yakushoku class
+class Yakushoku
+    def calculate_salary(kihonkyu)
+        kihonkyu
+    end
+
+    def up
+    end
+
+    def down
+    end
+end
+
+class Tanto < Yakushoku
+    def up
+        Shunin.new
+    end
+end
+
+class Shunin < Yakushoku
+    def calculate_salary(kihonkyu)
+        kihonkyu*2 + 1
+    end
+    
+    def down
+        Tanto.new
+    end
+end
