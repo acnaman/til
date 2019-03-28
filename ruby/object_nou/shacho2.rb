47ef387f82d8b63069a3889abb8a1a8684408d5f
acnaman <acnadin01@gmail.com>
2019-03-28 16:24:12 +0900

    template pattern

---
+++ ruby/object_nou/shacho2.rb
@@ -0,0 +1,12 @@
+# coding:utf-8
+require_relative "shain2"
+
+shain = Tanto.new(100)
+shain.print_salary
+
+shain = Shunin.new(100)
+shain.print_salary
+
+shain = Bucho.new(100)
+shain.print_salary
+
---
+++ ruby/object_nou/shain2.rb
@@ -0,0 +1,34 @@
+# coding: utf-8
+
+class Shain
+    def initialize(kihonkyu)
+        @kihonkyu = kihonkyu
+    end
+
+    def print_salary
+        puts "私の給料は#{calc_salary}です。"
+    end
+
+    def calc_salary
+    end
+end
+
+class Tanto < Shain
+    def calc_salary
+        @kihonkyu
+    end
+end
+
+class Shunin < Shain
+    def calc_salary
+        @kihonkyu * 2 + 1
+    end
+end
+
+class Bucho < Shain
+    def calc_salary
+        @kihonkyu * 3
+    end
+end
+
+
