312aba8a746b6aac69baab64cf83717feb651bdf
acnaman <acnadin01@gmail.com>
2020-04-12 15:52:25 +0900

    numpy & go

---
+++ Go/prac/section2/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"./animals"
+)
+
+func main() {
+	fmt.Println(AppName())
+
+	fmt.Println(animals.ElephantFeed())
+	fmt.Println(animals.RabbitFeed())
+	fmt.Println(animals.MonkeyFeed())
+}
