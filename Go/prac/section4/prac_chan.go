a8a8d7bfbf9f244d9409c19095ba98ec15cad083
acnaman <acnadin01@gmail.com>
2020-04-14 12:58:23 +0900

    section4 channel end

--- Go/prac/section4/prac_chan.go
+++ Go/prac/section4/prac_chan.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	func3()
+	func4()
 }
 
 func func1() {
@@ -75,6 +75,65 @@ func func3() {
 	}
 	close(ch)
 	time.Sleep(3 * time.Second)
+}
+
+func func4() {
+	ch1 := make(chan int, 1)
+	ch2 := make(chan int, 1)
+	ch3 := make(chan int, 10)
+
+	ch1 <- 1
+	ch2 <- 2
+
+LOOP:
+	for {
+		select {
+		case <-ch1:
+			fmt.Println("------ch1から受信------")
+		case <-ch2:
+			fmt.Println("------ch2から受信------")
+		case ch3 <- 3:
+			fmt.Println("------ch3へ送信------")
+		default:
+			fmt.Println("受信送信できません")
+			break LOOP
+		}
+	}
+}
+
+func func5() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	ch3 := make(chan int)
+
+	go func() {
+		for {
+			i := <-ch1
+			ch2 <- i * 2
+		}
+	}()
+
+	go func() {
+		for {
+			i := <-ch2
+			ch3 <- i - 1
+		}
+	}()
+
+	n := 1
+LOOP:
+	for {
+		select {
+		case ch1 <- n:
+			n++
+		case i := <-ch3:
+			fmt.Println("received", i)
+		default:
+			if n > 100 {
+				break LOOP
+			}
+		}
+	}
 }
 
 func receiver(ch <-chan int) {
