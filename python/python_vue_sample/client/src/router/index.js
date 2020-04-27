656b892b27c5eeb68c76c8df4dc79d597fc5f61c
acnaman <acnadin01@gmail.com>
2020-04-27 14:01:45 +0000

    refleshed vue project for unuse TypeScript

--- python/python_vue_sample/client/src/router/index.js
+++ python/python_vue_sample/client/src/router/index.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
+import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes: Array<RouteConfig> = [
+  const routes = [
   {
     path: '/',
     name: 'Home',
@@ -21,8 +21,6 @@ Vue.use(VueRouter)
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes
 })
 
