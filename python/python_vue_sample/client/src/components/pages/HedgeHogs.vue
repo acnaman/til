847ca382f37a80bf5581ec49a48203b28eacf1c0
acnaman <acnadin01@gmail.com>
2020-04-28 03:07:17 +0000

    WIP: login page

---
+++ python/python_vue_sample/client/src/components/pages/HedgeHogs.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    HedgeHog Page
+  </div>
+</template>
+
+<script>
+import router from "../../router/index"
+export default {
+  name: 'HedgeHogs',
+  mounted() {
+    this.checkLoggedIn();
+  },
+  methods: {
+    checkLoggedIn() {
+      router.push('/auth');
+    }
+  }
+};
+</script>
--- python/python_vue_sample/client/src/router/index.js
+++ python/python_vue_sample/client/src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import HedgeHogs from '../components/pages/HedgeHogs'
+import Auth from '../components/pages/Auth'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'HedgeHogs',
+    component: HedgeHogs
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/auth',
+    name: 'Auth',
+    component: Auth
   }
 ]
 
